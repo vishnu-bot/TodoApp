2ae84f250f658e1010aba2cd309122d6910aaf25
Vishnu <vishnuvijaykumar780@gmail.com>
2025-12-03 23:45:34 +0530

    Setup of Core Data model and UI works for the home page

---
+++ TodoApp/Resources/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  TodoApp
+//
+//  Created by Vishnu on 03/12/25.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let todayTaskString = "Today"
+    static let allTaskString = "All Tasks"
+    static let searchString = "Search"
+    static let dashboardString = "Dashboard"
+    
+    static let todayTaskIcon = "calendar"
+    static let allTaskIcon = "list"
+    static let searchIcon = "magnifyingglass"
+    static let dashboardIcon = "house"
+}
---
+++ TodoApp/ViewModel/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  TodoApp
+//
+//  Created by Vishnu on 03/12/25.
+//
+
+import Foundation
