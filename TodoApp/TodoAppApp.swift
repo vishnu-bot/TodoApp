1b281353b7daf3e3d8871c21ab480bbab4044b6d
Vishnu <vishnuvijaykumar780@gmail.com>
2025-12-03 22:52:28 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Vishnu on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
