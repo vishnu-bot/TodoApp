bf07eb837c66880995fb5c5cbf5596a64c87857e
Vishnu <vishnuvijaykumar780@gmail.com>
2025-12-04 14:25:51 +0530

    Addition of HomeView, ListView, NewTaskView

---
+++ TodoApp/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TodoApp
+//
+//  Created by Vishnu on 04/12/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
